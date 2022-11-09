0e834d9b90b4511e462f47e8ac0d43b2fcf527ff
Courtney Lippman <courtney.lippman@colorado.edu>
2022-11-09 10:53:48 -0700

    Add data to file.

---
+++ src/data/allBookings-data.js
@@ -0,0 +1,50 @@
+const allBookingsData = [
+    {
+        id: "5fwrgu4i7k55hl6sz",
+        userID: 9,
+        date: "2023/04/22",
+        roomNumber: 1
+        },
+        {
+        id: "5fwrgu4i7k55hl6t5",
+        userID: 43,
+        date: "2022/01/24",
+        roomNumber: 2
+        },
+        {
+        id: "5fwrgu4i7k55hl6t6",
+        userID: 13,
+        date: "2022/01/10",
+        roomNumber: 3
+        },
+        {
+        id: "5fwrgu4i7k55hl6t7",
+        userID: 20,
+        date: "2022/02/16",
+        roomNumber: 4
+        },
+        {
+        id: "5fwrgu4i7k55hl6t8",
+        userID: 1,
+        date: "2022/02/05",
+        roomNumber: 5
+        },
+        {
+        id: "5fwrgu4i7k55hl6t9",
+        userID: 38,
+        date: "2023/12/14",
+        roomNumber: 1
+        },
+        {
+        id: "5fwrgu4i7k55hl6ta",
+        userID: 25,
+        date: "2022/01/11",
+        roomNumber: 2
+        },
+        {
+        id: "5fwrgu4i7k55hl6tb",
+        userID: 49,
+        date: "2022/02/06",
+        roomNumber: 5
+        }
+];
