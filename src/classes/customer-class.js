c02663b666405ab4f0e3e086d5908f91dbf2a2ef
Courtney Lippman <courtney.lippman@colorado.edu>
2022-11-09 10:26:34 -0700

    Change method to match allRooms class methods.

--- src/classes/customer-class.js
+++ src/classes/customer-class.js
@@ -6,34 +6,23 @@ class Customer {
         this.pastBookings = [];
         this.allBookings = []
         this.totalAmountSpent = 0;
-        this.selectedDate = 00000000; //yearmonthday -> 20221109
-    }
-};
+        this.selectedDate = 00000000 //yearmonthday -> 20221109
+    };
 
-getBookings(SpecificBookingTypeListAllCustomers, bookingType) {
-   const customerListOfBookings = SpecificBookingTypeListAllCustomers.filter(booking => booking.userID === this.id);
-   if(bookingType === past) {
-        this.pastBookings = customerListOfBookings;
-    } else if(bookingType === future) {
-        this.upcomingBookings = customerListOfBookings;
-    } else if(bookingType === all) {
-        this.allBookings = customerListOfBookings
+    getBookings(SpecificBookingTypeListAllCustomers, bookingType) {
+    const customerListOfBookings = SpecificBookingTypeListAllCustomers.filter(booking => booking.userID === this.id);
+    if(bookingType === past) {
+            this.pastBookings = customerListOfBookings;
+        } else if(bookingType === future) {
+            this.upcomingBookings = customerListOfBookings;
+        } else if(bookingType === all) {
+            this.allBookings = customerListOfBookings
+        };
+    };
+
+    getTotalAmountSpent() {
+        return this.allBookings.reduce((acc, booking) => {
+        return acc += allRooms.getSpecificRoom(booking.roomNumber).costPerNight;
+        }, 0);
     };
 };
-
-getTotalAmountSpent() {
-    /*
-    iterate through this.allBookings array (reduce)
-        iterate through allRooms array
-            if room number of booking matches room number in allRooms array
-                then take that rooms costPerNight value and add it to acc
-        End iteration through allRooms array
-        return acc
-    End iteration through this.allBookings array
-    */
-    return this.allBookings.reduce((acc, booking) => {
-     return acc += allRooms.getSpecificRoom(booking.roomNumber).costPerNight
-    }, 0)
-}
-
-
