c1953e18c27c05047454a9da8942a9a6ea690547
Courtney Lippman <courtney.lippman@colorado.edu>
2022-11-10 20:08:18 -0700

    remove currentCustomer variable.

--- src/scripts.js
+++ src/scripts.js
@@ -5,5 +5,3 @@ import Customer from "./classes/customer-class";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-let currentCustomer;
