cb8b9189220b0ec00338b14e47c1bbf7f0ef55a6
Courtney Lippman <courtney.lippman@colorado.edu>
2022-11-11 08:58:35 -0700

    refactor customer class.

--- src/classes/customer-class.js
+++ src/classes/customer-class.js
@@ -1,29 +1,30 @@
 class Customer {
     constructor(singleCustomerInfo) {
         this.id = singleCustomerInfo.id;
-        this.name = singleCustomerInfo.name
+        this.customerName = singleCustomerInfo.name;
         this.upcomingBookings = [];
         this.pastBookings = [];
-        this.allBookings = []
-        this.totalAmountSpent = 0;
-        this.selectedDate = 00000000 //yearmonthday -> 20221109
+        this.allBookings = this.pastBookings + this.upcomingBookings;
+        this.totalAmountSpent = '';
     };
 
     getBookings(SpecificBookingTypeListAllCustomers, bookingType) {
-    const customerListOfBookings = SpecificBookingTypeListAllCustomers.filter(booking => booking.userID === this.id);
-    if(bookingType === past) {
+    let customerListOfBookings = SpecificBookingTypeListAllCustomers.filter(booking => booking.userID === this.id);
+    if(bookingType === 'past') {
             this.pastBookings = customerListOfBookings;
-        } else if(bookingType === future) {
+        } else if(bookingType === 'future') {
             this.upcomingBookings = customerListOfBookings;
-        } else if(bookingType === all) {
-            this.allBookings = customerListOfBookings
+        } else if(bookingType === 'all') {
+            this.allBookings = customerListOfBookings;
         };
     };
 
-    getTotalAmountSpent() {
-        return this.allBookings.reduce((acc, booking) => {
-        return acc += allRooms.getSpecificRoom(booking.roomNumber).costPerNight;
-        }, 0);
+    getTotalAmountSpent(allRooms) {
+        let aTotal = 0;
+        this.allBookings.forEach(booking => {
+            return aTotal+= allRooms.getSpecificRoom(booking.roomNumber).costPerNight;
+        })
+        this.totalAmountSpent = aTotal.toFixed(2);
     };
 };
 
