c5bc213ae6102d55a9ee4a2cc068eb542558eb38
Courtney Lippman <courtney.lippman@colorado.edu>
2022-11-11 12:37:27 -0700

    Create api fetch functionality.

---
+++ src/apiCalls.js
@@ -0,0 +1,27 @@
+let fetchData = (data) => {
+    return fetch(`http://localhost:3001/api/v1/${dataset}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error (`${response.status}`)
+        }
+        return response.json();
+    });
+};
+
+let postData = (data, customerData) => {
+    const dataRequest = {
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(customerData)
+    };
+
+    return fetch(`http://localhost:3001/api/v1/${dataset}`, dataRequest)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error (`${response.status}`);
+        }
+        return response.json();
+    });
+};
+
+export { fetchData, postData};
