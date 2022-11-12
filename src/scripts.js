49defb5cb967b62743ee9fe7442f4cddee765d42
Courtney Lippman <courtney.lippman@colorado.edu>
2022-11-12 13:29:02 -0700

    Add load functionalities for dashboard.

--- src/scripts.js
+++ src/scripts.js
@@ -10,44 +10,104 @@ import { fetchData, postData } from './apiCalls';
 import './images/turing-logo.png'
 
 // Global Variables ----------------------------------------------------------
-let listOfCustomers;
+let customersData;
+let allRoomsData;
+let allBookingsData;
 let allRooms;
 let allBookings;
-let login;
 let currentCustomer;
-// DOM Variables -------------------------------------------------------------
+let login;
+// DOM Query Selectors -------------------------------------------------------------
+//Dashboard Query Selectors --------------
+const datePickerInput = document.querySelector('#date-selector');
+const findRoomButton = document.querySelector('.search-avail-button-js');
+const totalSpentTxt = document.querySelector('.total-spent-txt');
+const thumbnailsUpcomingSection = document.querySelector('.thumbnails-of-upcoming-section-js');
+const thumbnailsPastSection = document.querySelector('.thumbnails-of-past-section-js');
+// Availible Rooms Query Selectors -------------
+
+//Fetch/Promise Functions -------------------
+
 
 // Event Listeners -----------------------------------------------------------
-
+//Dashboard Event Listeners  --------------
+    window.addEventListener('load', pageLoad)
+// datePickerInput.addEventListener('click', );
+// findRoomButton.addEventListener('click');
 // Functions -----------------------------------------------------------------
 
-function createLogin(username, password) {
-    loadData()
-    login = new Login(username, password)
-    login.createLoginStatus(listOfCustomers)
-    currentCustomer = login.currentCustomer;
-    console.log('allRooms', allRooms)
-    console.log('allBookings',allBookings)
-    console.log('currentCustomer',currentCustomer)
-    console.log('login', login)
-}
+// function createLogin(username, password) {
+//     loadData('.')
+//     login = new Login(username, password)
+//     login.createLoginStatus(listOfCustomers)
+//     currentCustomer = login.currentCustomer;
+//     console.log('allRooms', allRooms)
+//     console.log('allBookings',allBookings)
+//     console.log('currentCustomer',currentCustomer)
+//     console.log('login', login)
+// }
 
 
-
-function loadData() {
+function pageLoad() {
     Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
-    .then(allDataList => {
-        createDataSets(allDataList);
+    .then((allDataList) => {
+    
+        customersData = allDataList[0].customers;
+            allRoomsData = allDataList[1].rooms;
+            allBookingsData = allDataList[2].bookings;
+            console.log('customersData', customersData)
+            console.log('allRoomsData', allRoomsData )
+            console.log('allBookingsData', allBookingsData)
+            createInstances(allRoomsData, allBookingsData, customersData)
+            displayTotal()
+
+        
     })
     .catch(error => {
-        displayGetError(error);
+        console.log('error',error);
     })
+    
 }
 
-function createDataSets(allDataList) {
-    listOfCustomers = allDataList[0];
-    allRooms = new AllRooms(allDataList[1]);
-    allBookings = new AllBookings(allDataList[2]);
+function createInstances(allRoomsData, allBookingsData, customersData) {
+    let randomIndex;
+    console.log('allRoomsData', allRoomsData)
+    allRooms = new AllRooms(allRoomsData)
+    allBookings = new AllBookings(allBookingsData)
+    randomIndex = createRandomIndex(customersData)
+    currentCustomer = new Customer(customersData[randomIndex])
+    let todaysDate = allBookings.getTodayDate()
+    allBookings.sortBookings(todaysDate)
+    currentCustomer.getBookings(allBookings.allBookings, 'all')
+    currentCustomer.getBookings(allBookings.allPastBookings, 'past')
+    currentCustomer.getBookings(allBookings.allUpcomingBookings, 'future')
+    displayTotal()
+    displayStay(thumbnailsUpcomingSection, currentCustomer.upcomingBookings, 'upcoming')
+    displayStay(thumbnailsPastSection, currentCustomer.pastBookings, 'past')
+}
+
+function createRandomIndex(allCustomers) {
+    return Math.floor(Math.random() * allCustomers.length);
+  }
+
+function displayTotal() {
+    currentCustomer.getTotalAmountSpent(allRooms)
+   return totalSpentTxt.innerText = `$${currentCustomer.totalAmountSpent}`
+}
+
+function displayStay(domVar, bookingList, stayType) {
+    let staySection = "";
+    bookingList.forEach(booking => {
+         staySection += `<div class="single-avail-room single--stay-js">
+        <ul class="single-avail-room-list single-${stayType}-stay-list-js">
+          <li class="details booking-id">Booking ID: ${booking.id}</li>
+          <li class="details booking-date">Booking Date: ${booking.date}</li>
+          <li class="details booking-room">Room Number: ${booking.roomNumber}</li>
+        </ul>
+      </div>`
+    });
+
+    return domVar.innerHTML = staySection;
 }
 
 
