0176a8fe93dd7ea596cd078ccc0b8308db484163
Courtney Lippman <courtney.lippman@colorado.edu>
2022-11-13 20:11:14 -0700

    Change variable names.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -15,7 +15,7 @@ let postData = (data, customerData) => {
         body: JSON.stringify(customerData)
     };
 
-    return fetch(`http://localhost:3001/api/v1/${dataset}`, dataRequest)
+    return fetch(`http://localhost:3001/api/v1/${data}`, dataRequest)
     .then(response => {
         if(!response.ok) {
             throw new Error (`${response.status}`);
