ba45b3cfed4e806762b21c45e71fd2a0a88bd423
Courtney Lippman <courtney.lippman@colorado.edu>
2022-11-14 16:45:55 -0700

    Clean up console.logs and comments.

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,12 @@
 //Imports--------------------------------------------------------------------
 import './css/styles.css';
-import Customer from "./classes/customer-class";
 import Login from './classes/login-class';
 import AllRooms from './classes/allRooms-class';
 import AllBookings from './classes/AllBookings-class';
 import { fetchData, postData } from './apiCalls';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
 
 // Global Variables ----------------------------------------------------------
 let customersData;
@@ -19,62 +18,44 @@ let currentCustomer;
 let todaysDate;
 let selectedDate;
 let login;
+
 // DOM Query Selectors -------------------------------------------------------------
 // Login Query Selectors -------------
-const loginMain = document.querySelector('.login-main-js')
+const loginMain = document.querySelector('.login-main-js');
 const username = document.querySelector('#username');
 const password = document.querySelector('#password');
-const signInButton = document.querySelector('.sign-in-button-js')
-const loginSection = document.querySelector('.input-section-js')
-const loginErrorSection = document.querySelector('.login-error-section-js')
+const signInButton = document.querySelector('.sign-in-button-js');
+const loginSection = document.querySelector('.input-section-js');
+const loginErrorSection = document.querySelector('.login-error-section-js');
 //Dashboard Query Selectors --------------
-const DashboardPage = document.querySelector('.dash-main-js')
-const welcomeTxt = document.querySelector('.welcome-txt-js')
+const DashboardPage = document.querySelector('.dash-main-js');
+const welcomeTxt = document.querySelector('.welcome-txt-js');
 const datePickerInput = document.querySelector('#date-selector');
-const datePickerAlertWrapper = document.querySelector('.date-select-alert-wrapper-js')
-const datePickerAvailAlertWrapper = document.querySelector('.date-select-avail-alert-wrapper-js')
+const datePickerAlertWrapper = document.querySelector('.date-select-alert-wrapper-js');
+const datePickerAvailAlertWrapper = document.querySelector('.date-select-avail-alert-wrapper-js');
 const findRoomButton = document.querySelector('.search-avail-button-js');
 const totalSpentTxt = document.querySelector('.total-spent-txt');
 const thumbnailsUpcomingSection = document.querySelector('.thumbnails-of-upcoming-section-js');
 const thumbnailsPastSection = document.querySelector('.thumbnails-of-past-section-js');
-
 // Availible Rooms Query Selectors -------------
 const thumbnailsAvailSection = document.querySelector('.avail-room-full-displays-js');
-const availibleRoomsPage = document.querySelector('.availible-main-js')
-const goBackButton = document.querySelector('.go-to-dash-button-js')
-const allRoomTypesFilterOptions = document.querySelector('.filter-select')
-const filterSearchButton = document.querySelector('.filter-button-js')
-const bookButton = document.querySelector('.book-room-button')
-
-//Fetch/Promise Functions -------------------
-
+const availibleRoomsPage = document.querySelector('.availible-main-js');
+const goBackButton = document.querySelector('.go-to-dash-button-js');
+const allRoomTypesFilterOptions = document.querySelector('.filter-select');
+const filterSearchButton = document.querySelector('.filter-button-js');
 
 // Event Listeners -----------------------------------------------------------
 // Login Event Listeners -------------
-window.addEventListener('load', pageLoad)
-signInButton.addEventListener('click', verifyCredentials)
+window.addEventListener('load', pageLoad);
+signInButton.addEventListener('click', verifyCredentials);
 //Dashboard Event Listeners  --------------
-    findRoomButton.addEventListener('click', displayAvailRooms)
-    goBackButton.addEventListener('click', backToDash)
-
+findRoomButton.addEventListener('click', displayAvailRooms);
+goBackButton.addEventListener('click', backToDash);
 //Availible Rooms Page Event Listeners ------------
-filterSearchButton.addEventListener('click', displayRoomsOfSameType)
-thumbnailsAvailSection.addEventListener('click', requestBooking)
+filterSearchButton.addEventListener('click', displayRoomsOfSameType);
+thumbnailsAvailSection.addEventListener('click', requestBooking);
 
 // Functions -----------------------------------------------------------------
-
-// function createLogin(username, password) {
-//     loadData('.')
-//     login = new Login(username, password)
-//     login.createLoginStatus(listOfCustomers)
-//     currentCustomer = login.currentCustomer;
-//     console.log('allRooms', allRooms)
-//     console.log('allBookings',allBookings)
-//     console.log('currentCustomer',currentCustomer)
-//     console.log('login', login)
-// }
-
-
 function pageLoad() {
     Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
     .then((allDataList) => {
@@ -83,70 +64,67 @@ function pageLoad() {
             allBookingsData = allDataList[2].bookings;   
     })
     .catch(error => {
-        displayGetError(error)
+        displayGetError(error);
     })   
 }
 
 function displayGetError(error) {
     loginMain.innerHTML = `<h1 class="get-error-message">Whoops! Something went wrong. Please try again later! Error: ${error.message}.<h1>`;
-}
+};
 
 function verifyCredentials(event) {
     event.preventDefault(event)
-    login = new Login(username.value, password.value)
-    login.createLoginStatus(customersData)
-    console.log('login.loginStatus',login.loginStatus)
+    login = new Login(username.value, password.value);
+    login.createLoginStatus(customersData);
     if(login.loginStatus === 'accepted') {
-        createCustomerInfo()
-        displayDashBoard()
+        createCustomerInfo();
+        displayDashBoard();
     } else {
-        displayCredentialError()
-    }
-
-}
+        displayCredentialError();
+    };
+};
 
 function displayCredentialError() {
-    loginSection.classList.add('hide')
-    signInButton.classList.add('hide')
-    loginErrorSection.classList.remove('hide')
-    setTimeout(resetLogin, 2000)
-}
+    loginSection.classList.add('hide');
+    signInButton.classList.add('hide');
+    loginErrorSection.classList.remove('hide');
+    setTimeout(resetLogin, 2000);
+};
 
 function resetLogin() {
-    username.value = ''
-    password.value = ''
-    loginSection.classList.remove('hide')
-    signInButton.classList.remove('hide')
-    loginErrorSection.classList.add('hide')
+    username.value = '';
+    password.value = '';
+    loginSection.classList.remove('hide');
+    signInButton.classList.remove('hide');
+    loginErrorSection.classList.add('hide');
 
-}
+};
 
 function displayDashBoard() {
-    loginMain.classList.add('hide')
-    DashboardPage.classList.remove('hide')
-    console.log('currentCustomer',currentCustomer)
-    welcomeTxt.innerText = `Welcome ${currentCustomer.customerName}!`
-}
+    loginMain.classList.add('hide');
+    DashboardPage.classList.remove('hide');
+    welcomeTxt.innerText = `Welcome ${currentCustomer.customerName}!`;
+};
 
 function createCustomerInfo() {
-    allRooms = new AllRooms(allRoomsData)
-    allBookings = new AllBookings(allBookingsData)
-    currentCustomer = login.currentCustomer
-    todaysDate = allBookings.getTodayDate()
-    allBookings.sortBookings(todaysDate)
-    currentCustomer.getBookings(allBookings.allBookings, 'all')
-    currentCustomer.getBookings(allBookings.allPastBookings, 'past')
-    currentCustomer.getBookings(allBookings.allUpcomingBookings, 'future')
-    populateTypeFilter()
-    displayTotal()
-    displayStay(thumbnailsUpcomingSection, currentCustomer.upcomingBookings, 'upcoming')
-    displayStay(thumbnailsPastSection, currentCustomer.pastBookings, 'past')
-}
+    allRooms = new AllRooms(allRoomsData);
+    allBookings = new AllBookings(allBookingsData);
+    currentCustomer = login.currentCustomer;
+    todaysDate = allBookings.getTodayDate();
+    allBookings.sortBookings(todaysDate);
+    currentCustomer.getBookings(allBookings.allBookings, 'all');
+    currentCustomer.getBookings(allBookings.allPastBookings, 'past');
+    currentCustomer.getBookings(allBookings.allUpcomingBookings, 'future');
+    populateTypeFilter();
+    displayTotal();
+    displayStay(thumbnailsUpcomingSection, currentCustomer.upcomingBookings, 'upcoming');
+    displayStay(thumbnailsPastSection, currentCustomer.pastBookings, 'past');
+};
 
 function displayTotal() {
-    currentCustomer.getTotalAmountSpent(allRooms)
-   return totalSpentTxt.innerText = `$${currentCustomer.totalAmountSpent}`
-}
+    currentCustomer.getTotalAmountSpent(allRooms);
+   return totalSpentTxt.innerText = `$${currentCustomer.totalAmountSpent}`;
+};
 
 function displayStay(domVar, bookingList, stayType) {
     let staySection = "";
@@ -157,49 +135,44 @@ function displayStay(domVar, bookingList, stayType) {
           <li class="details booking-date">Booking Date: ${booking.date}</li>
           <li class="details booking-room">Room Number: ${booking.roomNumber}</li>
         </ul>
-      </div>`
-    });
-
+      </div>`;
+    })
     return domVar.innerHTML = staySection;
-}
+};
 
 function populateTypeFilter() {
     const typesList = allRooms.createListOfRoomTypes();
-    allRoomTypesFilterOptions.innerHTML = `<option value="all-room-type">All Room Types</option>`
+    allRoomTypesFilterOptions.innerHTML = `<option value="all-room-type">All Room Types</option>`;
     typesList.forEach((type) => {
       allRoomTypesFilterOptions.innerHTML += `
       <option class="tag-options-text hover" value="${type}">${type}</option>
     `;
     });
-  }
-
-//Event Listener Functions (outside of load)
-
+  };
 
 function displayAvailRooms(event) {
-    //may be an issue that our promise is in the displayPage function above
-event.preventDefault()
-selectedDate = datePickerInput.value
+event.preventDefault();
+selectedDate = datePickerInput.value;
 let userInput = parseInt(selectedDate.replaceAll('-', ''));
 if(userInput < todaysDate) {
     datePickerAlertWrapper.classList.remove('hide')
 } else {
  allBookings.sortAllAvailibleRooms(userInput, allRooms.allRooms)
 createAvailRoomThumbnailsDisplay(allBookings.allAvailableRooms)
-}
+};
 datePickerInput.value="yyyy-MM-dd";
 };
 
 function createAvailRoomThumbnailsDisplay(list) {
     if(list.length > 0) {
-        let availRoomsSection = ""
+        let availRoomsSection = "";
         list.forEach(availRoom => {
             let bidet;
             if(availRoom.bidet) {
-                bidet = 'Yes'
+                bidet = 'Yes';
             } else if(!availRoom.bidet) {
-                bidet = 'No'
-            }
+                bidet = 'No';
+            };
             availRoomsSection += `
                 <section class="single-avail-room single-avail-room-js">
                     <p class="single-avail-room-title">Room ${availRoom.number}</p>
@@ -211,32 +184,31 @@ function createAvailRoomThumbnailsDisplay(list) {
                         <li class="details room-costpernight"> Cost per Night: $${availRoom.costPerNight}</li>
                     </ul>
                     <button class="book-room-button" id ="${availRoom.number}">Book</button>
-                </section>`
-        })
-        DashboardPage.classList.add('hide')
-        availibleRoomsPage.classList.remove('hide')
-        thumbnailsAvailSection.classList.remove('hide')
-        return thumbnailsAvailSection.innerHTML = availRoomsSection
+                </section>`;
+        });
+        DashboardPage.classList.add('hide');
+        availibleRoomsPage.classList.remove('hide');
+        thumbnailsAvailSection.classList.remove('hide');
+        return thumbnailsAvailSection.innerHTML = availRoomsSection;
     } else {
-        datePickerAvailAlertWrapper.classList.remove('hide')
-    }
-}
+        datePickerAvailAlertWrapper.classList.remove('hide');
+    };
+};
 
 function displayRoomsOfSameType(event) {
-        event.preventDefault()
+        event.preventDefault();
         let availRoomsFiltered;
-        availRoomsFiltered = allBookings.filterByType(allRoomTypesFilterOptions.value);
-        console.log('availRoomsFiltered.length',availRoomsFiltered.length)
+        availRoomsFiltered = allBookings.filterByType(allRoomTypesFilterOptions.value)
         if(availRoomsFiltered.length > 0) {
             createAvailRoomThumbnailsDisplay(availRoomsFiltered);
         } else {
-            thumbnailsAvailSection.innerHTML = `<p class="no-avail-for-type-alert"> Our deepest apologies! There are currently no rooms available for the room type you selected. Please select a different room type or select the go back to dashboard button and try a different date. We apologize for the inconvenience.</p>`
-        }
+            thumbnailsAvailSection.innerHTML = `<p class="no-avail-for-type-alert"> Our deepest apologies! There are currently no rooms available for the room type you selected. Please select a different room type or select the go back to dashboard button and try a different date. We apologize for the inconvenience.</p>`;
+        };
       
-}
+};
 
 function requestBooking(event) {
-    event.preventDefault(event)
+    event.preventDefault(event);
     const customerRequest = {
         userID: currentCustomer.id,
         date: selectedDate.replaceAll('-', '/'),
@@ -245,55 +217,53 @@ function requestBooking(event) {
 
     postData('bookings', customerRequest)
         .then(response => {
-            displayPostSuccess()
-            setTimeout(updateBookingsData, 2000)
+            displayPostSuccess();
+            setTimeout(updateBookingsData, 2000);
          
         })
         .catch(error => {
-            displayPostError(error)
-            setTimeout(redisplayBookingsData, 3000)
-        })
-}
+            displayPostError(error);
+            setTimeout(redisplayBookingsData, 3000);
+        });
+};
 
 function updateBookingsData() {
     fetchData('bookings')
     .then(data => {
-        allBookings = new AllBookings (data.bookings) ;
-        allBookings.sortBookings(todaysDate)
-        allBookings.sortAllAvailibleRooms(parseInt(selectedDate.replaceAll('-', '')), allRooms.allRooms)
-        console.log('allBookings.allAvailableRooms', allBookings.allAvailableRooms)
-        populateTypeFilter()
-        redisplayBookingsData()
-      })
-}
+        allBookings = new AllBookings (data.bookings);
+        allBookings.sortBookings(todaysDate);
+        allBookings.sortAllAvailibleRooms(parseInt(selectedDate.replaceAll('-', '')), allRooms.allRooms);
+        populateTypeFilter();
+        redisplayBookingsData();
+      });
+};
 
 function displayPostSuccess() {
-    thumbnailsAvailSection.innerHTML = `<p class="booked-message">The Room Has Been Booked!</p>`
-}
+    thumbnailsAvailSection.innerHTML = `<p class="booked-message">The Room Has Been Booked!</p>`;
+};
 
 function displayPostError(error) {
     if(error.message[0] === '5') {
-        thumbnailsAvailSection.innerHTML = `<p class="booked-message">Whoops! Something is wrong with the server. Please try booking this room again later!</p>`
+        thumbnailsAvailSection.innerHTML = `<p class="booked-message">Whoops! Something is wrong with the server. Please try booking this room again later!</p>`;
     } else {
-        thumbnailsAvailSection.innerHTML = `<p class="booked-message">Server cannot process this request. Please try again later!</p>`
-    }
-
-}
+        thumbnailsAvailSection.innerHTML = `<p class="booked-message">Server cannot process this request. Please try again later!</p>`;
+    };
+};
 
 function redisplayBookingsData() {
     if(allBookings.allAvailableRooms.length > 0) {
-        createAvailRoomThumbnailsDisplay(allBookings.allAvailableRooms)
-        } else {
-            thumbnailsAvailSection.innerHTML = `<p class="no-avail-for-type-alert"> Our deepest apologies! There are no longer any rooms available for the date you selected. Please select the go back to dashboard button and try a different date. We apologize for the inconvenience.</p>`
-        }
-}
+        createAvailRoomThumbnailsDisplay(allBookings.allAvailableRooms);
+    } else {
+        thumbnailsAvailSection.innerHTML = `<p class="no-avail-for-type-alert"> Our deepest apologies! There are no longer any rooms available for the date you selected. Please select the go back to dashboard button and try a different date. We apologize for the inconvenience.</p>`;
+    };
+};
 
 function backToDash (event) {
     event.preventDefault(event);
-    DashboardPage.classList.remove('hide')
-    availibleRoomsPage.classList.add('hide')
-    datePickerAlertWrapper.classList.add('hide')
-    datePickerAvailAlertWrapper.classList.add('hide')
-}
+    DashboardPage.classList.remove('hide');
+    availibleRoomsPage.classList.add('hide');
+    datePickerAlertWrapper.classList.add('hide');
+    datePickerAvailAlertWrapper.classList.add('hide');
+};
 
 
